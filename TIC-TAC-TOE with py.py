e7abb7a7f45fd2a3fcfc41291bdc5554353a2499
Tiham69 <105205393+Tiham69@users.noreply.github.com>
2023-04-24 15:07:43 +0600

    Update TIC-TAC-TOE with py.py

--- TIC-TAC-TOE with py.py
+++ TIC-TAC-TOE with py.py
@@ -6,17 +6,26 @@ def print_board(board):
 
 
 def is_winning_move(board, player):
+
     # Check rows
+    # ---
+
     for i in range(3):
         if (board[3 * i] == player and board[3 * i + 1] == player and board[3 * i + 2] == player):
             return True
 
     # Check columns
+    # |||
+
     for i in range(3):
         if (board[i] == player and board[i + 3] == player and board[i + 6] == player):
             return True
 
     # Check diagonals
+    #   / and \
+    #  /       \
+    # /         \ give and take
+
     if (board[0] == player and board[4] == player and board[8] == player):
         return True
     elif (board[2] == player and board[4] == player and board[6] == player):
@@ -36,14 +45,14 @@ def play_game():
     board = [" "] * 9
 
     while True:
-        # Player 1's turn
+        # Player 1 turn
         print_board(board)
-        move = int(input("Player 1, enter your move (1-9): "))
-        board[move - 1] = "X"
+        move = int(input("First Person enter your move (1-9): "))
+        board[move - 1] = "T"
 
-        if is_winning_move(board, "X"):
+        if is_winning_move(board, "T"):
             print_board(board)
-            print("Player 1 wins!")
+            print("\'First Person\' wins! -wooo Tiham is the best. Eat shit second person ")
             break
 
         if is_tie(board):
@@ -51,14 +60,14 @@ def play_game():
             print("It's a tie!")
             break
 
-        # Player 2's turn
+        # Player 2 turn
         print_board(board)
-        move = int(input("Player 2, enter your move (1-9): "))
-        board[move - 1] = "O"
+        move = int(input("Second Person enter your move (1-9): "))
+        board[move - 1] = "L"
 
-        if is_winning_move(board, "O"):
+        if is_winning_move(board, "L"):
             print_board(board)
-            print("Player 2 wins!")
+            print("\'Second\' Person wins!")
             break
 
         if is_tie(board):
